62c5a7a7bdf454b56ecb73e5dfb0357a71758526
Elshad-Hasanli <75389204+Elshad-Hasanli@users.noreply.github.com>
2021-01-10 17:46:23 +0400

    Spring Boot empty, mutlimodule project is created

---
+++ blog-site-database/src/main/java/com/hasanli/BlogSiteDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.hasanli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogSiteDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogSiteDatabaseApplication.class, args);
+	}
+
+}
---
+++ blog-site-web/src/main/java/com/hasanli/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.hasanli;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BlogSiteWebApplication.class);
+	}
+
+}
