c6a72e2aefb6e24f48363d44779dddd676fe1270
Elshad-Hasanli <75389204+Elshad-Hasanli@users.noreply.github.com>
2021-01-25 00:47:13 +0400

    final commit

--- blog-site-multimodule/blog-site-database/src/main/java/com/hasanli/BlogSiteDatabaseApplication.java
+++ blog-site-multimodule/blog-site-database/src/main/java/com/hasanli/BlogSiteDatabaseApplication.java
@@ -1,21 +1,11 @@
 package com.hasanli;
 
-import com.hasanli.entity.*;
-import com.hasanli.service.inter.*;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.sql.Timestamp;
-import java.util.List;
 
 @SpringBootApplication
 public class BlogSiteDatabaseApplication {
-
-	@Autowired
-	SubscribersServiceInter subscribersServiceInter;
 	public static void main(String[] args) {
 		SpringApplication.run(BlogSiteDatabaseApplication.class, args);
 	}
